7873e90d2de6e8014f9934a65152b8ea2c9cc342
Francisco Raposo <fraposo@redhat.com>
2026-06-12 12:33:37 -0400

    Add per-cluster banner and quick console links in visualizer
    Render a top banner identifying the current cluster and show configurable quick links to broker consoles and consumer endpoints.

--- apps/visualizer/src/main/java/com/redhat/demo/visualizer/IndexResource.java
+++ apps/visualizer/src/main/java/com/redhat/demo/visualizer/IndexResource.java
@@ -13,6 +13,16 @@ public class IndexResource {
 
   @ConfigMapping(prefix = "demo.ui")
   public interface UiConfig {
+    /**
+     * Banner shown on the top of the page to visually identify
+     * which cluster/site this instance of the visualizer is running on.
+     */
+    @WithDefault("AMQ1")
+    String bannerLabel();
+
+    @WithDefault("#2563eb")
+    String bannerColor();
+
     @WithDefault("AMQ1")
     String amq1Label();
 
@@ -32,6 +42,27 @@ public class IndexResource {
     String amq3Color();
   }
 
+  @ConfigMapping(prefix = "demo.links")
+  public interface LinksConfig {
+    @WithDefault("")
+    String amq1BrokerConsoleUrl();
+
+    @WithDefault("")
+    String amq2BrokerConsoleUrl();
+
+    @WithDefault("")
+    String amq3BrokerConsoleUrl();
+
+    @WithDefault("")
+    String amq1ConsumerUrl();
+
+    @WithDefault("")
+    String amq2ConsumerUrl();
+
+    @WithDefault("")
+    String amq3ConsumerUrl();
+  }
+
   @ConfigMapping(prefix = "demo.consumers")
   public interface ConsumersConfig {
     /**
@@ -46,16 +77,25 @@ public class IndexResource {
   @Inject Template index;
   @Inject UiConfig cfg;
   @Inject ConsumersConfig consumers;
+  @Inject LinksConfig links;
 
   @GET
   public TemplateInstance home() {
     return index
+      .data("bannerLabel", cfg.bannerLabel())
+      .data("bannerColor", cfg.bannerColor())
       .data("amq1Label", cfg.amq1Label())
       .data("amq1Color", cfg.amq1Color())
       .data("amq2Label", cfg.amq2Label())
       .data("amq2Color", cfg.amq2Color())
       .data("amq3Label", cfg.amq3Label())
       .data("amq3Color", cfg.amq3Color())
+      .data("amq1BrokerConsoleUrl", links.amq1BrokerConsoleUrl())
+      .data("amq2BrokerConsoleUrl", links.amq2BrokerConsoleUrl())
+      .data("amq3BrokerConsoleUrl", links.amq3BrokerConsoleUrl())
+      .data("amq1ConsumerUrl", links.amq1ConsumerUrl())
+      .data("amq2ConsumerUrl", links.amq2ConsumerUrl())
+      .data("amq3ConsumerUrl", links.amq3ConsumerUrl())
       .data("amq1SnapshotUrl", consumers.amq1SnapshotUrl())
       .data("amq2SnapshotUrl", consumers.amq2SnapshotUrl())
       .data("amq3SnapshotUrl", consumers.amq3SnapshotUrl());
